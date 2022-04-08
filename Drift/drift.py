944a87b50deb1d520ee79c41d0254cfe4f2f7ec0
Memhir-Yasue <eyasu.woldu@temple.edu>
2022-04-08 17:20:39 -0400

    init

---
+++ Drift/drift.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+
+def drift(models, validation_set) -> pd.DataFrame:
+    # Ingest validation dataset
+    # Ingest models
+    # For each model, get model's prediction on validation set
+    # concat model predictions, and write to AWS or Local
+    ...
